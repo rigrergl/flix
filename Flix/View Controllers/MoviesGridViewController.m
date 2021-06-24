c79f3c5e356dc32c7c02957cd517d458479c7428
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-24 02:06:30 -0500

    started setting up collection view

---
+++ Flix/View Controllers/MoviesGridViewController.m
@@ -0,0 +1,58 @@
+//
+//  MoviesGridViewController.m
+//  Flix
+//
+//  Created by Rigre Reinier Garciandia Larquin on 6/24/21.
+//
+
+#import "MoviesGridViewController.h"
+
+@interface MoviesGridViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation MoviesGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+}
+
+- (void) fetchMovies {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+//               [self addNetworkErrorAlert];
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+               self.movies = dataDictionary[@"results"];
+               
+//               [self.tableView reloadData];
+           }
+//        [self.refreshControl endRefreshing];
+//        [self.activityIndicator stopAnimating];
+       }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
