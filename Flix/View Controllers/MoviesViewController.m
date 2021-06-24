58bbc0d2bbde8dba0b9bbadd40298e52456d7c0f
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-24 01:24:07 -0500

    finished network error alert

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -47,6 +47,7 @@
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
            if (error != nil) {
                NSLog(@"%@", [error localizedDescription]);
+               [self addNetworkErrorAlert];
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
@@ -63,6 +64,24 @@
         [self.activityIndicator stopAnimating];
        }];
     [task resume];
+}
+
+- (void) addNetworkErrorAlert{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies" message:@"The internet connection appears to be offline." preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // create a cancel action
+    UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // handle cancel response here. Doing nothing will dismiss the view.
+        NSLog(@"Try again");
+        [self viewDidLoad];
+        }];
+    
+    // add the cancel action to the alertController
+    [alert addAction:tryAgainAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
