a26b02ee8f0ce9daf8734e98dcf9d9780b38271a
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-24 20:04:41 -0500

    webkit setup

---
+++ Flix/View Controllers/TrailerViewController.h
@@ -0,0 +1,16 @@
+//
+//  TrailerViewController.h
+//  Flix
+//
+//  Created by Rigre Reinier Garciandia Larquin on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TrailerViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/View Controllers/TrailerViewController.m
@@ -0,0 +1,46 @@
+//
+//  TrailerViewController.m
+//  Flix
+//
+//  Created by Rigre Reinier Garciandia Larquin on 6/24/21.
+//
+
+#import "TrailerViewController.h"
+#import "WebKit/Webkit.h"
+
+@interface TrailerViewController ()
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
+
+@end
+
+@implementation TrailerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // As a property or local variable
+    NSString *urlString = @"https://www.dropbox.com/terms?mobile=1";
+    
+    // Convert the url String to a NSURL object.
+    NSURL *url = [NSURL URLWithString:urlString];
+
+    // Place the URL in a URL Request.
+    NSURLRequest *request = [NSURLRequest requestWithURL:url
+                                             cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
+                                         timeoutInterval:10.0];
+    // Load Request into WebView.
+    [self.webView loadRequest:request];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
