d0ff82eb1a4a6ae698bbb847fa85aa8d92864b9a
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-24 23:03:52 -0500

    fixed details segue bug that heppened when user searched

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -158,7 +158,7 @@
     UITableViewCell *tappedCell = sender;
     NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
     
-    NSDictionary *movie = self.movies[indexPath.row];
+    NSDictionary *movie = self.filteredMovies[indexPath.row];
     
     DetailsViewController *detailsViewController = [segue destinationViewController];
     
