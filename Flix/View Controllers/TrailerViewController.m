fd32e42778e04840dc2152ac6ca5e5ae99619217
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-24 20:31:30 -0500

    resized webview

--- Flix/View Controllers/TrailerViewController.m
+++ Flix/View Controllers/TrailerViewController.m
@@ -17,11 +17,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *baseYoutubeURL = @"https://www.youtube.com/watch?v=\SUXWAEX2jlg";
+    
     // Do any additional setup after loading the view.
     NSLog(@"Movie ID: %d", self.movie_id);
     
     // As a property or local variable
-    NSString *urlString = @"https://www.dropbox.com/terms?mobile=1";
+    NSString *urlString = baseYoutubeURL;
     
     // Convert the url String to a NSURL object.
     NSURL *url = [NSURL URLWithString:urlString];
